46c25b198a96f1f9e130e51688a9f50b5f0a17d0
Bureau536 <g.bureau15@gmail.com>
2024-10-24 19:18:28 -0300

    Class 73 Update
    Answer buttos updated

---
+++ lib/models/quiz_questions.dart
@@ -0,0 +1,9 @@
+
+
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answer);
+  
+  final String text;
+  final List<String> answer;
+
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_third_exercise/answer_button.dart';
+import 'package:flutter_third_exercise/data/questions.dart';
 
-class QuestionsScreen extends StatefulWidget{
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
-  
+
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreeState();
@@ -12,6 +14,30 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreeState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answer.map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
+      ),
+    );
   }
-  }
+}
