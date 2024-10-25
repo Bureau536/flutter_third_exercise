9e2683c593bf5001c11f31d74ded8d2d6f2766ca
Bureau536 <g.bureau15@gmail.com>
2024-10-25 20:05:45 -0300

    Lesson 85
    Added Answers List

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_third_exercise/data/questions.dart';
+import 'package:flutter_third_exercise/questions_summary.dart';
+
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
@@ -39,7 +40,7 @@ class ResultsScreen extends StatelessWidget {
           children: [
             const Text('You answered X out of Y questions correctly!'),
             const SizedBox(height: 30),
-            const Text('List of answers and questions'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(height: 30),
             TextButton(onPressed: () {}, child: const Text('Restart QUiz!'))
           ],
