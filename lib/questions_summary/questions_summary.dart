918b50bf76c26d572ca4edd037d6ca87ea22e39f
Bureau536 <g.bureau15@gmail.com>
2024-10-26 13:36:35 -0300

    Update
    Added Styling to Questions ans Answers on Summary Screen

--- lib/questions_summary/questions_summary.dart
+++ lib/questions_summary/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -14,16 +15,35 @@ class QuestionsSummary extends StatelessWidget {
           children: summaryData.map((data) {
             return Row(
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                Text(
+                  ((data['question_index'] as int) + 1).toString(),
+                ),
                 Expanded(
                   child: Column(
                     children: [
-                      Text(data['question'] as String),
+                      Text(
+                        data['question'] as String,
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(255, 213, 179, 250),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                       const SizedBox(
                         height: 5,
                       ),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
+                      Text(
+                        data['user_answer'] as String,
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(255, 202, 126, 126),
+                        ),
+                      ),
+                      Text(
+                        data['correct_answer'] as String,
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(255, 151, 180, 145),
+                        ),
+                      )
                     ],
                   ),
                 ),
--- lib/questions_summary/summary_item.dart
+++ lib/questions_summary/summary_item.dart
@@ -3,6 +3,10 @@
 import 'package:flutter/material.dart';
 
 class SummaryItem extends StatelessWidget{
+const SummaryItem ({super.key, required this.summaryItem,});
 
-  
+final String summaryItem;
+
+return SummaryItem
+
 }
